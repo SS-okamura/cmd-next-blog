2f3064432a42098a667eb0f2058a08f702be15cd
okamura0509 <milktea.chaba2bay@gmail.com>
2022-11-14 16:29:15 +0900

    一覧表示、詳細画面の実装

---
+++ pages/blog/[id].tsx
@@ -0,0 +1,40 @@
+import { client } from "../../libs/client";
+
+type Blog = {
+    id:number,
+    title:string,
+    body:string | number | File,
+  }
+
+export default function BlogId({ blog }:{blog:Blog}) {
+  return (
+    <main>
+      <h1>{blog.title}</h1>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `${blog.body}`,
+        }}
+      />
+    </main>
+  );
+}
+
+// 静的生成のためのパスを指定します
+export const getStaticPaths = async () => {
+  const data = await client.get({ endpoint: "blog" });
+
+  const paths = data.contents.map((content: { id: string; }) => `/blog/${content.id}`);
+  return { paths, fallback: false };
+};
+
+// データをテンプレートに受け渡す部分の処理を記述します
+export const getStaticProps = async (context: { params: { id: string; }; }) => {
+  const id = context.params.id;
+  const data = await client.get({ endpoint: "blog", contentId: id });
+
+  return {
+    props: {
+      blog: data,
+    },
+  };
+};
---
+++ type/blog.tsx
@@ -0,0 +1,6 @@
+import Link from "next/link";
+
+type Blog = {
+    title:string,
+    body:any
+}
